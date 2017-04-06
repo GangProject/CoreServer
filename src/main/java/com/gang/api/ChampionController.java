a94b16ddf8b448b2735ee59e5808684378759a64
sjaqjwor <lsklsk45@naver.com>
2017-04-07 04:26:50 +0900

    swagger

--- src/main/java/com/gang/api/ChampionController.java
+++ src/main/java/com/gang/api/ChampionController.java
@@ -2,9 +2,12 @@ package com.gang.api;
 
 import com.gang.domain.Champion.ChampionEntity;
 import com.gang.domain.Champion.ChampionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.rithms.riot.api.RiotApiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,13 +16,15 @@ import java.util.List;
  * Created by seungki on 2017-04-07.
  */
 @RestController
-@RequestMapping(value = "Champion")
+@Api(value = "챔피언 정보", description = "챔피언 API", basePath= "/api/champion")
+@RequestMapping(value = "/api/champion")
 public class ChampionController {
 
     @Autowired
     private ChampionService championService;
 
-    @RequestMapping("/save")
+    @RequestMapping(value = "/save",method = RequestMethod.GET)
+    @ApiOperation(value = " 현재 롤 챔피언 정보", notes = "현재 롤 챔피언 정보 저장")
     public List<ChampionEntity> ChampionSave() throws RiotApiException {
         try {
             return championService.list();
