e2f5ed751e767b96807ab4305225644ab7571595
chucky6413 <chucky6413@naver.com>
2017-04-09 19:06:13 +0900

    [junwoo] 챌린저 리그 api 추가

--- src/main/java/com/gang/core/manager/LeagueApiManager.java
+++ src/main/java/com/gang/core/manager/LeagueApiManager.java
@@ -4,6 +4,7 @@ import com.gang.core.RiotApiManager;
 import com.gang.core.StringNotFoundException;
 import net.rithms.riot.api.RateLimitException;
 import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.constant.QueueType;
 import net.rithms.riot.constant.Region;
 import net.rithms.riot.dto.League.League;
 import org.springframework.stereotype.Component;
@@ -39,5 +40,26 @@ public class LeagueApiManager extends RiotApiManager{
         }
 
         return leagues;
+    }
+    //챌린저 리그 조회
+    public League getChallengerLeague(Region region,QueueType queueType) throws StringNotFoundException,InterruptedException{
+        League league = null;
+        Boolean success = false;
+
+        while(!success){ //key 횟수 초과될 수 있으므로 , 돌리기용.
+            System.out.println(key.toString());
+            try {
+                league = api.getChallengerLeague(region, queueType);
+                success = true;
+            } catch (RateLimitException e) { //key 요청 횟수 초과시
+                System.out.println("key 바꿈");
+                changeKey();
+                Thread.sleep(1000); //1초 동안 sleep
+            } catch (RiotApiException e) {
+                System.out.println(e);
+            }
+        }
+
+        return league;
     }
 }
