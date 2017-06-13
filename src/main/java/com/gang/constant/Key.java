d80b35b6dad285e587a2d001031617e5194a11eb
sjaqjwor <lsklsk45@naver.com>
2017-06-13 19:01:38 +0900

    하이

--- src/main/java/com/gang/constant/Key.java
+++ src/main/java/com/gang/constant/Key.java
@@ -7,9 +7,12 @@ import net.rithms.riot.api.RiotStringNotFound;
  * Created by Junwoo on 2017-04-07.
  */
 public enum Key {
-    KEYONE(0,"RGAPI-e8372943-2ac3-4bed-8d2c-86f9c86174fe"),
-    KEYTWO(1,"RGAPI-342e18e7-3f6f-42ca-8eaf-063956bd50ad"),
-    KEYTHREE(2,"RGAPI-3a7ff858-0698-4186-8093-708a986da6dc");
+    KEYONE(4,"RGAPI-ee2bd3aa-238e-409c-88db-24a3b745773"),
+    KEYTWO(3,"RGAPI-342e18e7-3f6f-42ca-8eaf-063956bd50ad"),
+    KEYTHREE(2,"RGAPI-3a7ff858-0698-4186-8093-708a986da6dc"),
+    KEYFOUR(1,"RGAPI-43d14c22-daa7-4869-adbf-8dc0bafe27bb"),
+    KEYFIVE(0,"RGAPI-b1d04bb3-a207-47cd-9807-3939277ae77a");
+
 
     private int id;
     private String key;
